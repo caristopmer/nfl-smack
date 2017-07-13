beff2d783319a24a7cdaa96e64b928df9615a18d
Chris McMenomy <email": null>
2017-07-13 13:00:34 -0500

    migrations written, seeds in progress.

---
+++ db/seeds.rb
@@ -0,0 +1,84 @@
+# Team Seeds
+Team.create(location: "Buffalo", name: "Bills", color_hex: "")
+Team.create(location: "Miami", name: "Dolphins", color_hex: "")
+Team.create(location: "New England", name: "Patriots", color_hex: "")
+Team.create(location: "New York", name: "Jets", color_hex: "")
+
+Team.create(location: "Baltimore", name: "Ravens", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+
+
+
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+Team.create(location: "", name: "", color_hex: "")
+
+Team.create(location: "Atlanta", name: "Falcons", color_hex: "")
+Team.create(location: "Carolina", name: "Panthers", color_hex: "")
+Team.create(location: "New Orleans", name: "Saints", color_hex: "")
+Team.create(location: "Tampa Bay", name: "Buccaneers", color_hex: "")
+
+Team.create(location: "Arizona", name: "Cardinals", color_hex: "")
+Team.create(location: "Los Angeles", name: "Rams", color_hex: "")
+Team.create(location: "San Francisco", name: "49ers", color_hex: "")
+Team.create(location: "Seattle", name: "Seahawks", color_hex: "")
+
+
+
+
+
+
+
+
+
+  "Buffalo": "Bills"
+  "Miami": "Dolphins"
+  "New England": "Patriots"
+  "New York": "Jets"
+  "Baltimore": "Ravens"
+  "Cincinnati": "Bengals"
+  "Cleveland": "Browns"
+  "Pittsburgh": "Steelers"
+  "Houston": "Texans"
+  "Indianapolis": "Colts"
+  "Jacksonville": "Jaguars"
+  "Tennessee": "Titans"
+  "Denver": "Broncos"
+  "Kansas City": "Chiefs"
+  "Oakland": "Raiders"
+  "Los Angeles": "Chargers"
+  "Dallas": "Cowboys"
+  "New York": "Giants"
+  "Philadelphia": "Eagles"
+  "Washington": "Redskins"
+  "Chicago": "Bears"
+  "Detroit": "Lions"
+  "Green Bay": "Packers"
+  "Minnesota": "Vikings"
+
+
+# User Seeds
+
+
+# Post Seeds
+
+
+# Reply Seeds
