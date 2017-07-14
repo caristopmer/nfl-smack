88c38b7dbbb3687d193cc698fbd29fee761b9196
Chris McMenomy <email": null>
2017-07-14 12:36:17 -0500

    post index styled, started ajaxing add new post.

--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-  
+  $('#new-post-form').on('submit', function() {
+    event.preventDefault()
+    var postData = $('this').serialize()
+    $.ajax({
+      url: '/posts',
+      type: 'POST',
+      data: postData,
+    })
+    .done(function(response) {
+      $('.posts-list').append(response);
+
+      $('#new-post-form').reset()
+      console.log("success");
+    })
+    .fail(function(response) {
+      console.log("error");
+    })
+  });
 });
 
