33690ada1d614ff904f95a6a50459b0f0b3e62ea
Chris McMenomy <email": null>
2017-07-14 13:14:56 -0500

    ajax working for submit post

--- public/js/application.js
+++ public/js/application.js
@@ -1,19 +1,23 @@
 $(document).ready(function() {
   $('#new-post-form').on('submit', function() {
-    event.preventDefault()
-    var postData = $('this').serialize()
+    event.preventDefault();
+    var postData = $('#new-post-form').serialize();
+    console.log(postData);
+
     $.ajax({
       url: '/posts',
       type: 'POST',
-      data: postData,
+      data: postData
     })
     .done(function(response) {
       $('.posts-list').append(response);
 
-      $('#new-post-form').reset()
+      $('#new-post-form')[0].reset()
       console.log("success");
     })
     .fail(function(response) {
+      var message = response.responseText;
+      alert(message);
       console.log("error");
     })
   });
